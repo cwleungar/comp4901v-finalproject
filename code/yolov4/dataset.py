1ede18371f6e69fab8228d71b35b49b58c66bf7c
cwleungar <cwleungar@connect.ust.hk>
2023-04-17 22:50:26 +0800

    Update dataset.py

--- code/yolov4/dataset.py
+++ code/yolov4/dataset.py
@@ -259,10 +259,8 @@ class Yolo_dataset(Dataset):
             truth[data[0]] = []
             for i in data[1:]:
                 temp=[]
-                if not i:
-                    continue
+                print("item ",i)
                 for j in i.split(','):
-                    print(j)
                     temp.append(int(float(j)))
                 truth[data[0]].append(temp)
 
