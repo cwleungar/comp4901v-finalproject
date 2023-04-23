e6de59f6c7349c58387427468dcae92bffccb434
cwleungar <cwleungar@connect.ust.hk>
2023-04-23 22:05:58 +0800

    Update train.py

--- code/yolov4/train.py
+++ code/yolov4/train.py
@@ -513,9 +513,9 @@ def evaluate(model, data_loader, cfg, device, logger=None, **kwargs):
             confs = confs.cpu().detach().numpy()
             labels = np.argmax(confs, axis=1).flatten()
             labels = torch.as_tensor(labels, dtype=torch.int64)
-            print("box:",list(boxes[0][0].cpu().detach().numpy()))
-            print("lb",labels[0].detach().numpy())
-            visiualize(img,boxes[0]+labels[0])
+            
+            boxes=list(boxes[0][0].cpu().detach().numpy()).append(labels[0].detach().numpy())
+            visiualize(img,boxes)
             raise('stop')
             scores = np.max(confs, axis=1).flatten()
             scores = torch.as_tensor(scores, dtype=torch.float32)
