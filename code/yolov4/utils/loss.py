54e577c4bf5ec48d4888e098a0cdc27bf5aa0896
cwleungar <cwleungar@connect.ust.hk>
2023-04-24 22:37:14 +0800

    Update loss.py

--- code/yolov4/utils/loss.py
+++ code/yolov4/utils/loss.py
@@ -166,7 +166,7 @@ def build_targets(p, targets, model):
 
         # Append
         #indices.append((b, a, gj, gi))  # image, anchor, grid indices
-        indices.append((b, a, gj.clamp_(0, gain[3] - 1), gi.clamp_(0, gain[2] - 1)))  # image, anchor, grid indices
+        indices.append((int(b), int(a), int(gj.clamp_(0, gain[3] - 1)), int(gi.clamp_(0, gain[2] - 1))))  # image, anchor, grid indices
         tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
         anch.append(anchors[a])  # anchors
         tcls.append(c)  # class
