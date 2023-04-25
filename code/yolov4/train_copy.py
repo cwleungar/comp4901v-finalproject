4c3d5092a26bed9512bf01a9995027ca942d1523
cwleungar <cwleungar@connect.ust.hk>
2023-04-25 19:31:52 +0800

    Update train_copy.py

--- code/yolov4/train_copy.py
+++ code/yolov4/train_copy.py
@@ -35,7 +35,8 @@ import torch.nn as nn
 import yaml
 from torch.optim import lr_scheduler
 from tqdm import tqdm
-
+import sys
+sys.path.append('..')
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv3 root directory
 if str(ROOT) not in sys.path:
