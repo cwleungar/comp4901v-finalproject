e05069914d62da018ac3b506606fed7ef46cd948
cwleungar <cwleungar@connect.ust.hk>
2023-04-25 20:00:15 +0800

    Update train.py

--- code/yolov4_new/train.py
+++ code/yolov4_new/train.py
@@ -304,16 +304,21 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             scaler.scale(loss).backward()
 
             # Optimize - https://pytorch.org/docs/master/notes/amp_examples.html
-            if ni - last_opt_step >= accumulate:
-                scaler.unscale_(optimizer)  # unscale gradients
-                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=10.0)  # clip gradients
+            #if ni - last_opt_step >= accumulate:
+            #    scaler.unscale_(optimizer)  # unscale gradients
+            #    torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=10.0)  # clip gradients
+            #    scaler.step(optimizer)  # optimizer.step
+            #    scaler.update()
+            #    optimizer.zero_grad()
+            #    if ema:
+            #        ema.update(model)
+            #    last_opt_step = ni
+            if ni % accumulate == 0:
                 scaler.step(optimizer)  # optimizer.step
                 scaler.update()
                 optimizer.zero_grad()
                 if ema:
                     ema.update(model)
-                last_opt_step = ni
-
             # Log
             if RANK in {-1, 0}:
                 mloss = (mloss * i + loss_items) / (i + 1)  # update mean losses
