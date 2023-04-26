38c660c4b52743573e534d8dc00677c29b0d13fd
cwleungar <cwleungar@connect.ust.hk>
2023-04-27 02:32:44 +0800

    Update deep_sort.py

--- code/deep_sort/deep_sort/deep_sort.py
+++ code/deep_sort/deep_sort/deep_sort.py
@@ -110,7 +110,9 @@ class DeepSort(object):
         for box in bbox_xywh:
             x1,y1,x2,y2 = self._xywh_to_xyxy(box)
             im = ori_img[y1:y2,x1:x2]
-            print("bbox ",bbox_xywh)
+            print("box ",box)
+            print("x1,y1,x2,y2 ",x1,y1,x2,y2)
+            print("im ",im)
             if im.size == 0:
                 continue
             im_crops.append(im)
