36b4485a843757c2be48f7f3080579d547c81ccd
cwleungar <cwleungar@connect.ust.hk>
2023-04-26 23:38:26 +0800

    Update __init__.py

--- code/deep_sort/detector/__init__.py
+++ code/deep_sort/detector/__init__.py
@@ -3,6 +3,8 @@ from .YOLOv3.utils.general import intersect_dicts
 from .YOLOv4.models.models import Darknet as YOLOv4
 from .YOLOv5.models.yolo import Model as YOLOv5
 import torch
+import yaml
+
 __all__ = ['build_detector']
 
 def build_detector(cfg, use_cuda):
@@ -11,6 +13,9 @@ def build_detector(cfg, use_cuda):
         ckpt = torch.load(cfg.YOLOV3.WEIGHT, map_location='cpu')
         cfgr=cfg.YOLOV3.CFG
         hyp=cfg.YOLOV3.HYP
+        if isinstance(hyp, str):
+            with open(hyp, errors='ignore') as f:
+                hyp = yaml.safe_load(f) 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model = YOLOv3(cfgr or ckpt['model'].yaml, ch=3, nc=8, anchors=hyp.get('anchors')).to(device)  # create
         exclude = ['anchor'] if (cfgr or hyp.get('anchors')) else []  # exclude keys
@@ -31,6 +36,9 @@ def build_detector(cfg, use_cuda):
         ckpt = torch.load(cfg.YOLOV5.WEIGHT, map_location='cpu')
         cfgr=cfg.YOLOV5.CFG
         hyp=cfg.YOLOV5.HYP
+        if isinstance(hyp, str):
+            with open(hyp, errors='ignore') as f:
+                hyp = yaml.safe_load(f) 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model = YOLOv5(cfgr or ckpt['model'].yaml, ch=3, nc=8, anchors=hyp.get('anchors')).to(device)  # create
         exclude = ['anchor'] if (cfgr or hyp.get('anchors')) else []  # exclude keys
