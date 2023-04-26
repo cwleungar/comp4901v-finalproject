d5660a67655857fbe353e89f30b657f7ed607d0b
cwleungar <cwleungar@connect.ust.hk>
2023-04-26 23:09:23 +0800

    s

--- code/deep_sort/detector/YOLOv3/__init__.py
+++ code/deep_sort/detector/YOLOv3/__init__.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("detector/YOLOv3")
 
 
-from .detector import YOLOv3
+#from .detector import YOLOv3
 __all__ = ['YOLOv3']
 
 
--- code/deep_sort/detector/YOLOv5/__init__.py
+++ code/deep_sort/detector/YOLOv5/__init__.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("detector/YOLOv5")
 
 
-from .detector import YOLOv5
+#from .detector import YOLOv5
 __all__ = ['YOLOv5']
 
 
