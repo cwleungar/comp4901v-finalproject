6f9f5b8eebb9278af2facb6c97bddbcaa01de8f8
cwleungar <cwleungar@connect.ust.hk>
2023-04-27 02:20:00 +0800

    Update yolo_deepsort.py

--- code/deep_sort/yolo_deepsort.py
+++ code/deep_sort/yolo_deepsort.py
@@ -172,7 +172,7 @@ class VideoTracker(object):
                 bbox = np.array(xywh, dtype=np.float32).reshape(1, 4)
                 bbox_xywh[int(cls)] = np.concatenate([bbox_xywh[int(cls)], bbox], axis=0)
                 cls_conf[int(cls)] = np.concatenate([cls_conf[int(cls)], np.array([conf], dtype=np.float32)], axis=0)
-            
+            print(cls_conf)
             #bbox_xywh, cls_conf, cls_ids = self.detector(im)
 
             # select person class
