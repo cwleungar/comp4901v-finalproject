afce9abdfa7ea0c157f6ff334df3ed5f05a989a6
cwleungar <cwleungar@connect.ust.hk>
2023-04-27 11:11:34 +0800

    Update __init__.py

--- code/deep_sort/detector/__init__.py
+++ code/deep_sort/detector/__init__.py
@@ -1,8 +1,8 @@
 from detector.YOLOv3.utils.torch_utils import smart_inference_mode
 from .YOLOv3.models.yolo import DetectMultiBackend as YOLOv3
 from .YOLOv3.utils.general import intersect_dicts
-from .YOLOv4.models.models import Darknet as YOLOv4
-from .YOLOv5.models.yolo import Model as YOLOv5
+from .YOLOv4.models.models import DetectMultiBackend as YOLOv4
+from .YOLOv5.models.yolo import DetectMultiBackend as YOLOv5
 import torch
 import yaml
 
@@ -28,26 +28,13 @@ def build_detector(cfg, use_cuda):
         return model,getname(cfg.YOLOV3.CLASS_NAMES)
     elif 'YOLOV4' in cfg:
         device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
-        ckpt = torch.load(cfg.YOLOV4.WEIGHT, map_location=device)  # load checkpoint
-        cfgr=cfg.YOLOV4.CFG
-        hyp=cfg.YOLOV4.HYP
-        model = YOLOv4(cfgr).to(device)  # create
-        state_dict=ckpt['model'].state_dict()
-        model.load_state_dict(state_dict, strict=False)
-        model=model.eval()
+
+        model = YOLOv4(cfg.YOLOV3.WEIGHT, dnn=False,device=device,data="/data/cwleungar/comp4901v-finalproject/code/yolov3/data/dataset.yaml",fp16=False)
+
         return model,getname(cfg.YOLOV4.CLASS_NAMES)
     elif 'YOLOV5' in cfg:
-        ckpt = torch.load(cfg.YOLOV5.WEIGHT, map_location='cpu')
-        cfgr=cfg.YOLOV5.CFG
-        hyp=cfg.YOLOV5.HYP
-        if isinstance(hyp, str):
-            with open(hyp, errors='ignore') as f:
-                hyp = yaml.safe_load(f) 
         device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
-        model = YOLOv5(cfgr or ckpt['model'].yaml, ch=3, nc=9, anchors=hyp.get('anchors')).to(device)  # create
-        exclude = ['anchor'] if (cfgr or hyp.get('anchors')) else []  # exclude keys
-        csd = ckpt['model'].float().state_dict()  # checkpoint state_dict as FP32
-        csd = intersect_dicts(csd, model.state_dict(), exclude=exclude)  # intersect
-        model.load_state_dict(csd, strict=False)  # load
-        model=model.eval()
+
+        model = YOLOv5(cfg.YOLOV3.WEIGHT, dnn=False,device=device,data="/data/cwleungar/comp4901v-finalproject/code/yolov3/data/dataset.yaml",fp16=False)
+
         return model,getname(cfg.YOLOV5.CLASS_NAMES)
