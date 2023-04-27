a86579398130412ce9e64ec93d2dce15e563815b
cwleungar <cwleungar@connect.ust.hk>
2023-04-27 11:19:50 +0800

    Update __init__.py

--- code/deep_sort/detector/__init__.py
+++ code/deep_sort/detector/__init__.py
@@ -1,7 +1,7 @@
 from detector.YOLOv3.utils.torch_utils import smart_inference_mode
 from .YOLOv3.models.yolo import DetectMultiBackend as YOLOv3
 from .YOLOv3.utils.general import intersect_dicts
-from .YOLOv4.models.common import DetectMultiBackend as YOLOv4
+from .YOLOv4.models.models import Darknet as YOLOv4, load_darknet_weights
 from .YOLOv5.models.common import DetectMultiBackend as YOLOv5
 import torch
 import yaml
@@ -27,9 +27,13 @@ def build_detector(cfg, use_cuda):
         
         return model,getname(cfg.YOLOV3.CLASS_NAMES)
     elif 'YOLOV4' in cfg:
+        cfgr=cfg.YOLOV4.CFG
+        ckpt = torch.load(cfg.YOLOV4.WEIGHT, map_location=device)
         device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
-
-        model = YOLOv4(cfg.YOLOV4.WEIGHT, dnn=False,device=device,data="/data/cwleungar/comp4901v-finalproject/code/yolov3/data/dataset.yaml",fp16=False)
+        model = YOLOv4(cfgr,640).to(device)  # create
+        state_dict=ckpt['model'].state_dict()
+        model.load_state_dict(state_dict, strict=False)
+        model = YOLOv4(cfgr, 640)
 
         return model,getname(cfg.YOLOV4.CLASS_NAMES)
     elif 'YOLOV5' in cfg:
