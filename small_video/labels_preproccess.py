c30c5bba926fdedfdfbc17eb61a13f180defedf2
cwleungar <cwleungar@connect.ust.hk>
2023-05-08 17:24:26 +0800

    Update labels_preproccess.py

--- small_video/labels_preproccess.py
+++ small_video/labels_preproccess.py
@@ -15,6 +15,8 @@ def convert_kitti_to_mot(kitti_dir, mot_dir):
                     line = line.strip().split()
                     frame_id = int(line[0])
                     obj_id = int(line[1])
+                    if obj_id == -1:
+                        continue
                     x, y, w, h = map(float, line[6:10])
                     conf = -1  # KITTI does not provide detection confidence scores
                     cls = -1   # KITTI does not provide object class labels
